2b7ae03af97c6370c20795b2e4828de2dfb537a5
Saimanoj8 <saimanojkoyalkar08@gmail.com>
2022-05-14 20:38:50 +0530

    revert program

---
+++ src/test/java/Com/Vtiger/Testcases/Revert.java
@@ -0,0 +1,9 @@
+package Com.Vtiger.Testcases;
+
+public class Revert {
+
+	public static void main(String[] args) {
+System.out.println("revert");
+	}
+
+}
---
+++ src/test/java/Com/Vtiger/Testcases/Simple_Hello.java
@@ -0,0 +1,10 @@
+package Com.Vtiger.Testcases;
+
+public class Simple_Hello {
+
+	public static void main(String[] args) {
+
+		System.out.println("hello world");
+	}
+
+}
